0eafad17952204de038f125996b92381f2fd93dd
nagerenxiong <527935649@qq.com>
2016-06-13 02:29:05 +0800

    20160613

--- content.js
+++ content.js
@@ -1,60 +1,48 @@
-var url = window.location.href;
-$.ajax({
-	type: "get",
-	timeout: 6000,
-	async: true,
-	url: "https://favorite.taobao.com/popup/add_collection.htm?spm=2013.1.1000126.d22.oUVOn6&id=100090892&itemid=100090892&itemtype=0&ownerid=e71f91b7b5d4190e2f0b7ebc51630896&scjjc=2&_tb_token_=DyoKBwEFXI6GQQ",
-	success: function(data) {
-		console.log(data);
-	}
-});
-$.ajax({
-	type: "post",
-	timeout: 6000,
-	async: true,
-	url: "http://ruyi.taobao.com/my/feed/want",
-	data:{
-		url:'https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-14143658827.2.jrHLz3&id=532885696316',
-		source:'ext'
-	},
-	success: function(data) {
-		console.log(data);
-	}
-});
-$.ajax({
-	type: "post",
-	timeout: 6000,
-	async: true,
-	url: "http://ruyi.taobao.com/my/feed/want",
-	data:{
-		url:'https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-14143658827.14.jrHLz3&id=533705559424',
-		source:'ext'
-	},
-	success: function(data) {
-		console.log(data);
-	}
-});
-$.ajax({
-	type: "post",
-	timeout: 6000,
-	async: true,
-	url: "http://ruyi.taobao.com/my/feed/want",
-	data:{
-		url:'https://item.taobao.com/item.htm?spm=a1z10.1-c.w4004-14143658827.12.jrHLz3&id=533768178206',
-		source:'ext'
-	},
-	success: function(data) {
-		console.log(data);
-	}
-});
-
-
-// chrome.runtime.sendMessage({
-// 	'className': className,
-// 	'fag': 0
+// $.ajax({
+// 	type: "get",
+// 	timeout: 6000,
+// 	async: true,
+// 	url: "https://favorite.taobao.com/popup/add_collection.htm?spm=2013.1.1000126.d22.oUVOn6&id=100090892&itemid=100090892&itemtype=0&ownerid=e71f91b7b5d4190e2f0b7ebc51630896&scjjc=2&_tb_token_=DyoKBwEFXI6GQQ",
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
 // });
-// chrome.runtime.sendMessage({
-// 	fag: 1
-// }, function(response) {
-// 	className = response.className;
+
+// setInterval(function(){
+// 	$.ajax({
+// 	type: "get",
+// 	timeout: 6000,
+// 	async: true,
+// 	url: "https://favorite.taobao.com/popup/add_collection.htm?spm=2013.1.1000126.d22.b5jVgs&id=116257103&itemid=116257103&itemtype=0&ownerid=e23e3d963c2e2ab99ee581351a8150f0&scjjc=2&_tb_token_=DyoKBwEFXI6GQQ",
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
+// });
+// },3000)
+// $.ajax({
+// 	type: "get",
+// 	timeout: 6000,
+// 	async: true,
+// 	url: "https://favorite.taobao.com/popup/add_collection.htm?itemtype=1&scjjc=1&id=44526063400&_tb_token_=DyoKBwEFXI6GQQ",
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
+// });
+// $.ajax({
+// 	type: "get",
+// 	timeout: 6000,
+// 	async: true,
+// 	url: "https://favorite.taobao.com/popup/add_collection.htm?spm=2013.1.1000126.d22.4PPiwV&id=104660394&itemid=104660394&itemtype=0&ownerid=63f0b3f6da377f7ad7de4414d5fad989&scjjc=2&_tb_token_=DyoKBwEFXI6GQQ",
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
+// });
+// $.ajax({
+// 	type: "get",
+// 	timeout: 6000,
+// 	async: true,
+// 	url: "https://favorite.taobao.com/popup/add_collection.htm?spm=2013.1.1000126.d22.4PPiwV&id=104660394&itemid=104660394&itemtype=0&ownerid=63f0b3f6da377f7ad7de4414d5fad989&scjjc=2&_tb_token_=DyoKBwEFXI6GQQ",
+// 	success: function(data) {
+// 		console.log(data);
+// 	}
 // });
